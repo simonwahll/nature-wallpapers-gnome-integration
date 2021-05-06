108117964e2dd74f9e45417a92c28070b57786e4
Simon Vall <wahll@protonmail.ch>
2021-05-06 20:56:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir /usr/share/gnome-background-properties 2> /dev/null
-cp usr/share/gnome-background-properties/nature-wallpapers-backgrounds.xml /usr/share/gnome-background-properties/
+install -Dm644 usr/share/gnome-background-properties/nature-wallpapers-backgrounds.xml -t /usr/share/gnome-background-properties/
